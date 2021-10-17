38e0e96b7030c6fae8cc56922a3324396227110b
Florian Jung <flo@windfis.ch>
2021-10-18 00:04:03 +0200

    Update calibration constants

--- Configuration.h
+++ Configuration.h
@@ -928,7 +928,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 97.89 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1180,7 +1180,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -43, -16, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -43, -16, -1.33 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
